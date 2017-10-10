64f6495e44af80eac7934698082deffe0e4a9577
LourdesLoayza <30554218+LourdesLoayza@users.noreply.github.com>
2017-10-10 12:51:34 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,21 +5,21 @@ var card=prompt("ingresar el numero de tarjeta")
 function isValidCard(numbersCard){
 
 //Se declaran las variables para almacenar los datos ingresados previamente
-  var cardArray=[];
-  var cardReverse=[];
-  var cardCheck=[];
+var cardArray=[];
+var cardReverse=[];
+var cardCheck=[];
 
 //Los datos ingresados se ingresan en el cardArray
-    cardArray=Array.from(numbersCard);
+cardArray=Array.from(numbersCard);
 
 /*Los valores del primer arreglo se recorren de mayor a menor para ingresarlos de manera invertida
 mientras se van ingresando los valores estos son cambiados de tipo string a tipo numero para que
 se puedan hacer las operaciones
 */
 
-      for (i=cardArray.length-1;i>=0;i--){
-        cardReverse.push(parseInt(cardArray))
-      }
+   for (i=cardArray.length-1;i>=0;i--){
+      var newNumber=parseInt(numbersCard.charAt(i));
+      cardReverse.push(newNumber)}
 
 return cardReverse;
 alert (cardReverse)
